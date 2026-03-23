aab405bb6ab68e0e603403590491fda48421688b
Sachin <cssachin83@gmail.com>
2026-03-23 15:00:57 +0530

    Finish: Switched to Aiven PostgreSQL with SSL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,25 @@
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync(path.join(__dirname, '../ca.pem')).toString(),
+    }
+  }
 });
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('MySQL Connected (Local Instance)');
+    console.log('PostgreSQL Connected (Aiven Cloud)');
     await sequelize.sync({ alter: true });
   } catch (error) {
-    console.error('Unable to connect to the local database:', error.message);
-    console.log('Make sure MySQL is running and the "career_roadmap" database exists.');
+    console.error('Unable to connect to PostgreSQL:', error.message);
   }
 };
 
