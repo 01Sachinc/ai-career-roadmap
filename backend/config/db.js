4563555fa299b14e335de6b4c87fd349f0a600da
Sachin <cssachin83@gmail.com>
2026-03-23 14:52:04 +0530

    Update: Switched to local MySQL configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,13 +8,11 @@ const sequelize = new Sequelize(process.env.MYSQL_URL, {
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('MySQL Connected (via Sequelize)');
-    
-    // Sync models
+    console.log('MySQL Connected (Local Instance)');
     await sequelize.sync({ alter: true });
   } catch (error) {
-    console.error('Unable to connect to the database:', error.message);
-    process.exit(1);
+    console.error('Unable to connect to the local database:', error.message);
+    console.log('Make sure MySQL is running and the "career_roadmap" database exists.');
   }
 };
 
